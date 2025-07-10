3c8dfd8e86ea5f318e45e95cd5bb80c49b1742d9
James W <26191568+jgwoolley@users.noreply.github.com>
2025-07-10 00:16:49 -0400

    Moved some things to the tab

--- src/app/waypoints/WayPointComponent.tsx
+++ src/app/waypoints/WayPointComponent.tsx
@@ -1,16 +1,37 @@
 'use client';
 
 import CytoscapeComponent from "@/components/CytoscapeComponent";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Box, Tab, Tabs } from "@mui/material";
 import cytoscape from "cytoscape";
-import { useCallback, useRef, useState } from "react";
+import { PropsWithChildren, useCallback, useRef, useState } from "react";
+import PathStepsTable from "./PathStepsTable";
 import useWayPointEdges from "./useWayPointEdges";
 import { useWayPointGraph } from "./useWayPointGraph";
 import useWayPointStylesheet from "./useWayPointStylesheet";
-import { calculateDistance, PathStep, WayPoint } from "./utils";
+import { PathStep, WayPoint } from "./utils";
 import WayPointActiveButtons from "./WayPointActiveButtons";
 import WayPointEditDialog from "./WayPointEditDialog";
 import WayPointsDataGrid from "./WayPointsDataGrid";
+
+function CustomTabPanel({ children, currentTab, index }: PropsWithChildren<{ currentTab: number, index: number }>) {
+    return (
+        <div
+            role="tabpanel"
+            hidden={currentTab !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+        >
+            {currentTab === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 export default function WayPointComponent() {
     const [waypoints, setWayPoints] = useState<WayPoint[]>([]);
@@ -29,7 +50,7 @@ export default function WayPointComponent() {
         }
         cy.fit(eles, padding);
     };
-    
+
     const onZoom = (waypoint: WayPoint) => {
         const cy = cyRef.current;
         if (cy == undefined) {
@@ -60,12 +81,10 @@ export default function WayPointComponent() {
     const cyFunction = useCallback<((cy: cytoscape.Core) => void)>((cy) => {
         cyRef.current = cy; // Store Cytoscape instance
         cy.on('select', 'node', function (evt) { // Or 'click' for desktop
-            const node = evt.target;
-            console.log({
-                event: 'select node',
-                nodeid: node.id(),
-                nodedata: node.data(),
-            });
+            const node = waypoints.find(x => x.data.id === evt.target.id());
+            if(node) {
+                handleOpenEditDialog(node);
+            }
         })
         cy.on('select', 'edge', function (evt) { // Or 'click' for desktop
             const edge = evt.target;
@@ -77,79 +96,32 @@ export default function WayPointComponent() {
         });
     }, []);
 
+    const [currentTab, setCurrentTab] = useState(0);
+
+    const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
+        setCurrentTab(newValue);
+    };
+
     return (
         <div >
-            <h3>WayPoint Management</h3>
-            <h3>Graph</h3>
-            <div style={{
-                display: "flex",
-                position: "relative",
-                height: "50vh",
-                width: "100vw",
-                overflow: "hidden",
-            }}>
-                <div style={{
-                    flex: 1,
+            <h3>WayPoints</h3>
+            <CytoscapeComponent
+                elements={elements}
+                stylesheet={stylesheet}
+                minZoom={0.5}
+                maxZoom={2}
+                boxSelectionEnabled={false} // Disable box selection for cleaner interaction
+                wheelSensitivity={0.5} // Adjust zoom sensitivity
+                className="w-full" // Ensure it takes full width
+                autolock={true}
+                cy={cyFunction}
+                style={{
                     border: "solid #ddd",
-                    display: "flex",
-                    overflow: "hidden",
-                }}>
-                    <CytoscapeComponent
-                        elements={elements}
-                        stylesheet={stylesheet}
-                        minZoom={0.5}
-                        maxZoom={2}
-                        boxSelectionEnabled={false} // Disable box selection for cleaner interaction
-                        wheelSensitivity={0.5} // Adjust zoom sensitivity
-                        className="w-full" // Ensure it takes full width
-                        style={{ width: "100%", height: "100%", border: "solid #ddd" }} // Responsive height
-                        autolock={true}
-                        cy={cyFunction}
-                    />
-                </div>
+                    height: "50vh",
+                    width: "50vw",
+                }} 
+            />
 
-                <div style={{
-                    flex: 1,
-                    border: "solid #ddd",
-                    padding: "10px",
-                    overflowY: "auto",
-                }}>
-                    {(sourceNode != undefined && destinationNode != undefined && pathSteps != undefined) && (
-                        <>
-                            <h3>Steps</h3>
-                            <p>This is the path you will want to travel.</p>
-                            <TableContainer>
-                                <Table>
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>from</TableCell>
-                                            <TableCell>to</TableCell>
-                                            <TableCell>distance</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {pathSteps.map((x, index) => (<TableRow key={index}>
-                                            <TableCell sx={{cursor: "pointer"}} onClick={() => onZoomNode(x.from)}>{x.from.data("label")}</TableCell>
-                                            <TableCell sx={{cursor: "pointer"}} onClick={() => onZoomNode(x.to)}>{x.to.data("label")}</TableCell>
-                                            <TableCell>{Math.round(x.distance * 10) / 10}</TableCell>
-                                        </TableRow>))}
-                                        <TableRow>
-                                            <TableCell><b>Total</b></TableCell>
-                                            <TableCell><b></b></TableCell>
-                                            <TableCell><b>{pathSteps.reduce((acc, obj) => acc + obj.distance, 0)}</b></TableCell>
-                                        </TableRow>
-                                        <TableRow>
-                                            <TableCell><b>bird&#39;s eye distance</b></TableCell>
-                                            <TableCell><b></b></TableCell>
-                                            <TableCell><b>{Math.round(calculateDistance({ source: sourceNode, destination: destinationNode }))}</b></TableCell>
-                                        </TableRow>
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </>
-                    )}
-                </div>
-            </div>
             {/* Action Buttons */}
             <h4>WayPoint Actions</h4>
             <WayPointActiveButtons
@@ -160,24 +132,44 @@ export default function WayPointComponent() {
                 destinationNode={destinationNode}
                 setDestinationNode={setDestinationNode}
             />
-            <h4>Grid</h4>
-            {/* Waypoint Table */}
-            <WayPointsDataGrid
-                sourceNode={sourceNode}
-                destinationNode={destinationNode}
-                rows={waypoints}
-                onZoom={onZoom}
-                setSourceNode={setSourceNode}
-                setDestinationNode={setDestinationNode}
-                handleOpenEditDialog={handleOpenEditDialog}
-            />
+            <h3>WayPoint Management</h3>
+
+            <Box sx={{ width: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={currentTab} onChange={handleTabChange} aria-label="waypoint tabs">
+                        <Tab label="Table" {...a11yProps(0)} />
+                        <Tab label="Path" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+                <CustomTabPanel currentTab={currentTab} index={0}>
+                    <WayPointsDataGrid
+                        sourceNode={sourceNode}
+                        destinationNode={destinationNode}
+                        rows={waypoints}
+                        onZoom={onZoom}
+                        setSourceNode={setSourceNode}
+                        setDestinationNode={setDestinationNode}
+                        handleOpenEditDialog={handleOpenEditDialog}
+                    />
+                </CustomTabPanel>
+                <CustomTabPanel currentTab={currentTab} index={1}>
+                    <PathStepsTable 
+                        pathSteps={pathSteps}
+                        onZoomNode={onZoomNode}
+                        sourceNode={sourceNode}
+                        destinationNode={destinationNode}            
+                    />
+                </CustomTabPanel>
+
+            </Box>
+        
             <WayPointEditDialog
                 open={openEditDialog}
                 setOpen={setOpenEditDialog}
                 rows={waypoints}
                 setRows={setWayPoints}
                 editRow={editRow}
-                setEditRow={setEditRow}
+                setEditRow={setEditRow} // TODO: Move set source / dest into dialog?
             />
         </div>
     );
--- src/app/waypoints/WayPointEditDialog.tsx
+++ src/app/waypoints/WayPointEditDialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Autocomplete, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import { Autocomplete, Box, Button, ButtonGroup, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
 import { Dispatch, SetStateAction } from "react";
 import { stringifyWayPoint, WayPoint } from "./utils";
 
@@ -24,132 +24,171 @@ export default function WayPointEditDialog({ rows, setRows, open, setOpen, editR
     }
 
     const onSubmit = (event: React.FormEvent) => {
-        event.preventDefault(); 
+        event.preventDefault();
         handleClose();
-        setRows(prev => prev.map(prevRow => prevRow.data.id === editRow.data.id ? editRow: prevRow));
+        setRows(prev => prev.map(prevRow => prevRow.data.id === editRow.data.id ? editRow : prevRow));
     };
 
     return (
         <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Edit Waypoint</DialogTitle>
-                <DialogContent sx={{ paddingBottom: 0 }}>
-                    <DialogContentText>
-                        Please update the waypoint details below.
-                    </DialogContentText>
-                    <Box
+            <DialogTitle>Edit Waypoint</DialogTitle>
+            <DialogContent sx={{ paddingBottom: 0 }}>
+                <DialogContentText>
+                    Please update the waypoint details below.
+                </DialogContentText>
+                <Box
                     onSubmit={onSubmit}
-                        component="form"
-                        sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-                        noValidate
-                        autoComplete="off"
-                        >
-                        <TextField
-                            autoFocus
-                            required
-                            margin="dense"
-                            id="name"
-                            label="Waypoint"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            value={editRow.data.label}
-                            onChange={(x) => {
-                                setEditRow(prev => {
-                                    const newRow: WayPoint = {
-                                        ...prev,
-                                    };
-                                    newRow.data.label = x.target.value;
-                                    return newRow;
-                                });
-                            }}
-                        />
-                        <TextField
-                            required
-                            id="x"
-                            label="x"
-                            type="number"
-                            variant="standard"
-                            value={editRow.position.x}
-                            onChange={(x) => {
-                                const value = x.target.value.length === 0 ? 0 : parseFloat(x.target.value);
-                                if(value == undefined || isNaN(value)) {
-                                    return;
-                                }
+                    component="form"
+                    sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        margin="dense"
+                        id="name"
+                        label="id"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        disabled={true}
+                        value={editRow.data.id}
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        label="Waypoint"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={editRow.data.label}
+                        onChange={(x) => {
+                            setEditRow(prev => {
+                                const newRow: WayPoint = {
+                                    ...prev,
+                                };
+                                newRow.data.label = x.target.value;
+                                return newRow;
+                            });
+                        }}
+                    />
+                    <TextField
+                        required
+                        id="x"
+                        label="x"
+                        type="number"
+                        variant="standard"
+                        value={editRow.position.x}
+                        onChange={(x) => {
+                            const value = x.target.value.length === 0 ? 0 : parseFloat(x.target.value);
+                            if (value == undefined || isNaN(value)) {
+                                return;
+                            }
 
-                                setEditRow(prev => {
-                                    const newRow: WayPoint = {
-                                        ...prev,
-                                    };
-                                    newRow.position.x = value;
-                                    return newRow;
-                                });
-                            }}
-                        />
-
-                        <TextField
-                            required
-                            id="y"
-                            label="y"
-                            variant="standard"
-                            type="number"
-                            value={editRow.data.height}
-                            onChange={(x) => {
-                                const value = x.target.value.length === 0 ? 0 : parseFloat(x.target.value);
-                                if(value == undefined || isNaN(value)) {
-                                    return;
-                                }
-                                setEditRow(prev => {
-                                    const newRow: WayPoint = {
-                                        ...prev,
-                                    };
-                                    newRow.data.height = value;
-                                    return newRow;
-                                });
-                            }}
-                        />
-                        <TextField
-                            required
-                            id="z"
-                            label="z"
-                            variant="standard"
-                            type="number"
-                            value={editRow.position.y}
-                            onChange={(x) => {
-                                const value = x.target.value.length === 0 ? 0 : parseFloat(x.target.value);
-                                if(value == undefined || isNaN(value)) {
-                                    return;
-                                }
-                                setEditRow(prev => {
-                                    const newRow: WayPoint = {
-                                        ...prev,
-                                    };
-                                    newRow.position.y = value;
-                                    return newRow;
-                                });
-                            }}
-                        />
-                        <Autocomplete
-                            disablePortal
-                            options={rows}
-                            getOptionLabel={x => stringifyWayPoint(x)}
-                            sx={{ width: 300 }}
-                            value={ editRow.connection ? editRow.connection: null }
-                            onChange={(_, newValue) => {
-                                if(newValue != undefined) {
-                                    editRow.connection = newValue;
-                                    newValue.connection = editRow;
-                                }
-                            } }
-                            renderInput={(params) => <TextField {...params} label="Connection" />}
-                        />
-
-                        <DialogActions>
-                            <Button onClick={handleClose}>Cancel</Button>
-                            <Button onClick={handleDelete}>Remove</Button>
-                            <Button type="submit">Submit</Button>
-                        </DialogActions>
-                    </Box>
-                </DialogContent>
-            </Dialog>
+                            setEditRow(prev => {
+                                const newRow: WayPoint = {
+                                    ...prev,
+                                };
+                                newRow.position.x = value;
+                                return newRow;
+                            });
+                        }}
+                    />
+                    <TextField
+                        required
+                        id="y"
+                        label="y"
+                        variant="standard"
+                        type="number"
+                        value={editRow.data.height}
+                        onChange={(x) => {
+                            const value = x.target.value.length === 0 ? 0 : parseFloat(x.target.value);
+                            if (value == undefined || isNaN(value)) {
+                                return;
+                            }
+                            setEditRow(prev => {
+                                const newRow: WayPoint = {
+                                    ...prev,
+                                };
+                                newRow.data.height = value;
+                                return newRow;
+                            });
+                        }}
+                    />
+                    <TextField
+                        required
+                        id="z"
+                        label="z"
+                        variant="standard"
+                        type="number"
+                        value={editRow.position.y}
+                        onChange={(x) => {
+                            const value = x.target.value.length === 0 ? 0 : parseFloat(x.target.value);
+                            if (value == undefined || isNaN(value)) {
+                                return;
+                            }
+                            setEditRow(prev => {
+                                const newRow: WayPoint = {
+                                    ...prev,
+                                };
+                                newRow.position.y = value;
+                                return newRow;
+                            });
+                        }}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="origin"
+                        label="Origin"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        disabled={true}
+                        value={editRow.data.origin}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="createdTime"
+                        label="Created Time"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        disabled={true}
+                        value={editRow.data.createdTime}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="modifiedTime"
+                        label="Modified Time"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        disabled={true}
+                        value={editRow.data.modifiedTime}
+                    />
+                    <Autocomplete
+                        disablePortal
+                        options={rows}
+                        getOptionLabel={x => stringifyWayPoint(x)}
+                        sx={{ width: 300 }}
+                        value={editRow.connection ? editRow.connection : null}
+                        onChange={(_, newValue) => {
+                            if (newValue != undefined) {
+                                editRow.connection = newValue;
+                                newValue.connection = editRow;
+                            }
+                        }}
+                        renderInput={(params) => <TextField {...params} label="Connection" />}
+                    />
+                    <DialogActions>
+                        <ButtonGroup>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleDelete}>Remove</Button>
+                        <Button type="submit">Submit</Button></ButtonGroup>
+                    </DialogActions>
+                </Box>
+            </DialogContent>
+        </Dialog>
     )
 }
